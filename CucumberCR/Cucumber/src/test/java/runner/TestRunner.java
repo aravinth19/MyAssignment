5be1196363fa0dd916d5087f30e7c1112cbe8e2c
Aravinth Shanmugam <42541328+aravinth19@users.noreply.github.com>
2023-03-19 16:26:09 +0530

    Add files via upload

---
+++ CucumberCR/Cucumber/src/test/java/runner/TestRunner.java
@@ -0,0 +1,8 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "./src/test/java/features", glue = "steps", tags = "@CRs")
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
