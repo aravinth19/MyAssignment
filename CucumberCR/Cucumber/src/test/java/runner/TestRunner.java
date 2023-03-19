7bf9a76f7e97e00b00d828e1b7d87acd739132b7
Aravinth Shanmugam <42541328+aravinth19@users.noreply.github.com>
2023-03-19 16:27:43 +0530

    Delete CucumberCR/Cucumber directory

--- CucumberCR/Cucumber/src/test/java/runner/TestRunner.java
+++
@@ -1,8 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "./src/test/java/features", glue = "steps", tags = "@CRs")
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
